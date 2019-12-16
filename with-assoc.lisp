3321062160c5564ef2c3d879898af47d371e285f
Cyrus Harmon <cyrus@bobobeach.com>
2019-12-15 18:20:52 -0800

    auto keywordify symbols and remove test-not

--- with-assoc.lisp
+++ with-assoc.lisp
@@ -7,12 +7,12 @@
 
 (cl:in-package #:with-assoc)
 
-(defmacro with-assoc* ((associations &key key test test-not) alist &body body)
+(defmacro with-assoc* ((associations &key key test) alist &body body)
   "Takes a list containing a list of keyspec pairs (where keyspec is
 either 1) an atom key where key is a string or a keyword, or 2) a list
-of the form (key var)) and (optional) keyword args key test and
-test-not, an association list, and a body of code for which the
-variables will be bound to (cdr (assoc <key> alist))."
+of the form (key var)) and (optional) keyword args key and test, an
+association list, and a body of code for which the variables will be
+bound to (cdr (assoc <key> alist))."
   (let ((%alist (gensym "ALIST"))
         (pairvars (loop repeat (length associations)
                         collect (gensym "PAIR"))))
@@ -24,10 +24,13 @@ variables will be bound to (cdr (assoc <key> alist))."
                            (if (and value (listp value))
                                (car value)
                                value)))
-                      `(,pvar (assoc ,value ,%alist
+                      `(,pvar (assoc ,(if (and (symbolp value)
+                                               (not (keywordp value)))
+                                          (intern (symbol-name value) :keyword)
+                                          value)
+                                     ,%alist
                                      ,@(when key `(:key ,key))
-                                     ,@(when test `(:test ,test))
-                                     ,@(when test-not `(:test-not ,key)))))))
+                                     ,@(when test `(:test ,test)))))))
          (declare (ignorable ,@pairvars))
          (symbol-macrolet
              (,@(loop for var in associations
